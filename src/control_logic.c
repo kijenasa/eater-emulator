4e7ca5a102e7dbe4ceab46a318c2707052a107ce
julian <j.sp@tuta.io>
2025-01-25 10:37:36 -0500

    fix order of micro instructions

--- src/control_logic.c
+++ src/control_logic.c
@@ -20,18 +20,16 @@ void instruction_nop(cpu *c) {
 }
 
 void instruction_lda(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->memory_address_register);
 
   memory_out(c);
   register_in(c, &c->register_a);
+
+  instruction_fetch(c);
 }
 
 void instruction_add(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->memory_address_register);
 
@@ -40,11 +38,11 @@ void instruction_add(cpu *c) {
 
   alu_add(c);
   register_in(c, &c->register_a);
+
+  instruction_fetch(c);
 }
 
 void instruction_sub(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->memory_address_register);
 
@@ -53,59 +51,62 @@ void instruction_sub(cpu *c) {
 
   alu_sub(c);
   register_in(c, &c->register_a);
+
+  instruction_fetch(c);
 }
 
 void instruction_sta(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->memory_address_register);
 
   register_out(c, c->register_a);
   memory_in(c);
+
+  instruction_fetch(c);
 }
 
 void instruction_ldi(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->register_a);
+
+  instruction_fetch(c);
 }
 
 void instruction_jmp(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->instruction_register);
   register_in(c, &c->program_counter);
+
+  instruction_fetch(c);
 }
 
 void instruction_jc(cpu *c) {
-  instruction_fetch(c);
-
   if(c->flags[CARRY]) {
     register_out(c, c->instruction_register);
     register_in(c, &c->program_counter);
   }
+
+  instruction_fetch(c);
 }
 
 void instruction_jz(cpu *c) {
-  instruction_fetch(c);
-
   if(c->flags[IS_ZERO]) {
     register_out(c, c->instruction_register);
     register_in(c, &c->program_counter);
   }
+
+  instruction_fetch(c);
 }
 
 void instruction_out(cpu *c) {
-  instruction_fetch(c);
-
   register_out(c, c->register_a);
   register_in(c, &c->output_register);
+
+  instruction_fetch(c);
 }
 
 void instruction_hlt(cpu *c) {
+  c->halt = 1;
+
   instruction_fetch(c);
 
-  c->halt = 1;
 }
